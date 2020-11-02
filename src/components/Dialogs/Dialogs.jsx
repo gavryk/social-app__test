5da7ce87e9c10536acdb88c835ff59da6ac99e7b
Oleg <gvozdyk23@gmail.com>
2020-11-02 22:37:01 +0200

    'map-dialogs-and-messages'

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -7,7 +7,7 @@ const DialogItem = (props) => {
 
     return (
         <div className="dialog">
-            <NavLink to={ path }>{ props.name }</NavLink>
+            <NavLink className='border-bottom' to={ path }>{ props.name }<i className="fas fa-chevron-right"></i></NavLink>
         </div>
     )
 }
@@ -21,20 +21,40 @@ const Message = (props) => {
 }
 
 const Dialogs = () => {
+
+    let dialogsData = [
+        {id: 1, name: 'Oleg'},
+        {id: 2, name: 'Olena'},
+        {id: 3, name: 'Ivan'},
+        {id: 4, name: 'Max'},
+        {id: 5, name: 'John'}
+    ];
+
+    let messagesData = [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'What"s your name?'},
+        {id: 3, message: 'Yo'},
+        {id: 4, message: 'Hello'},
+        {id: 5, message: 'What?'}
+    ]
+
+    let dialogs = dialogsData.map((dialog) => {
+        return <DialogItem name={dialog.name} id={dialog.id} />
+    })
+
+    let messages = messagesData.map((message) => {
+        return <Message message={message.message} />
+    })
+
     return (
         <div className='dialog__block'>
             <h1 className='title pb-2 border-bottom'>Dialogs</h1>
             <div className="row">
                 <div className="dialogs col-4 border-right p-3">
-                    <DialogItem name='Oleg' id='1'/>
-                    <DialogItem name='John' id='2'/>
-                    <DialogItem name='Olena' id='3'/>
-                    <DialogItem name='Misha' id='4'/>
+                    {dialogs}
                 </div>
                 <div className="messages col-8 p-3">
-                    <Message message='Hi' />
-                    <Message message="What's your name?" />
-                    <Message message='Where are you?' />
+                    {messages}
                 </div>
             </div>
         </div>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,6 +4,19 @@ import './MyPosts.scss'
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+
+    let postData = [
+        {id: 1, message: 'Hi', like: 15},
+        {id: 2, message: 'What"s your name?', like: 20},
+        {id: 3, message: 'Yo', like: 35},
+        {id: 4, message: 'Hello', like: 150},
+        {id: 5, message: 'What?', like: 154}
+    ]
+
+    let posts = postData.map((post) => {
+        return <Post message={post.message} like={post.like}/>
+    })
+
     return(
         <div className='posts__wrapper'>
             <h2>My Posts</h2>
@@ -12,8 +25,7 @@ const MyPosts = () => {
                 <button className='btn btn-success'>Add Post</button>
             </div>
             <div className="posts-output pt-3">
-                <Post message="Hi" like='15'/>
-                <Post message="I'm fine" like='20'/>
+                { posts }
             </div>
         </div>
     )
